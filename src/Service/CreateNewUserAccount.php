976eb6b02795974e058cfb46da369c54db93b864
Brice Hessou <bricehessou@gmail.com>
2022-12-16 19:26:06 +0100

    fix: Integrity constraint violation: 1048 Column 'arrondissement_couvert_id' cannot be null

--- src/Service/CreateNewUserAccount.php
+++ src/Service/CreateNewUserAccount.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Entity\Arrondissement;
 use App\Entity\Utilisateur;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -17,11 +18,13 @@ final class CreateNewUserAccount
 
     public function create($email, $password, $role, $nom = null, $prenom = null): Utilisateur
     {
+        $arrondissement = $this->em->getRepository(Arrondissement::class)->findOneBy(['nom' => 'ABOMEY-CALAVI']);
         $user = new Utilisateur();
         $user->setEmail($email);
         $user->setPassword($this->encoder->hashPassword($user, $password));
         $user->setNom($nom)->setPrenoms($prenom);
         $user->setRoles($role);
+        $user->setArrondissementCouvert($arrondissement);
 
         $this->em->persist($user);
         $this->em->flush();
