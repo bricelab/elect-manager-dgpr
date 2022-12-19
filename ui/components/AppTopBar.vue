d03ccae8eeda756935cbdd67a26efbf3133e0569
Brice Hessou <bricehessou@gmail.com>
2022-12-20 00:14:53 +0100

    feat: Amélioration du front Agent

--- ui/components/AppTopBar.vue
+++ ui/components/AppTopBar.vue
@@ -17,18 +17,24 @@
     <v-menu v-if="isConnectedUserPage">
       <template v-slot:activator="{ props }">
 <!--        <v-btn icon="mdi-dots-vertical" v-bind="props" color="purple"></v-btn>-->
-        <v-avatar color="purple" v-bind="props">
-          <v-icon icon="mdi-account-circle"></v-icon>
-        </v-avatar>
+<!--        <v-avatar color="purple" v-bind="props" id="logout__menu" class="mr-2">-->
+<!--          <v-icon icon="mdi-account-circle"></v-icon>-->
+<!--        </v-avatar>-->
+        <a href="/logout" class="mr-3">
+          <v-icon icon="mdi-logout"></v-icon>
+        </a>
       </template>
 
-      <v-list color="purple" class="bg-purple mt-3">
-        <v-list-item>
-          <v-list-item-title>
-            <a href="/logout">Se déconnecter</a>
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
+<!--      <v-list color="purple" class="bg-purple mt-3">-->
+<!--        <v-list-item>-->
+<!--          <v-list-item-title>-->
+<!--            <a href="/logout">-->
+<!--              <v-icon icon="mdi-logout"></v-icon>-->
+<!--              Se déconnecter-->
+<!--            </a>-->
+<!--          </v-list-item-title>-->
+<!--        </v-list-item>-->
+<!--      </v-list>-->
     </v-menu>
 
   </v-app-bar>
@@ -48,4 +54,7 @@ defineProps({
 </script>
 
 <style scoped>
+#logout__menu {
+  cursor: pointer;
+}
 </style>
